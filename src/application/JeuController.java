7a66b55f4b09c28749d4b023d85fee2fc5ddebd9
Ali Lachguer <alilachguer95@gmail.com>
2016-12-11 15:53:27 +0100

    ajouter queleque sprite position

--- src/application/JeuController.java
+++ src/application/JeuController.java
@@ -1,20 +1,50 @@
 package application;
 
+import javafx.animation.Animation;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class JeuController {
+	private static final int COLUMNS  =   15;
+    private static final int COUNT    =  2;
+    private static final int OFFSET_X =  150;
+    private static final int OFFSET_Y =  50;
+    private static final int WIDTH    = 40;
+    private static final int HEIGHT   = 40;
 	private static final Image sprite_chien = new Image("/images/sprites/sprite_chien.png");
 	private static final Image sprite_chat = new Image("/images/sprites/sprite_chat.png");
 	private static final Image sprite_oiseau = new Image("/images/sprites/sprite_oiseau.png");
 	private static final Image sprite_bebe = new Image("/images/sprites/sprite_bebe.png");
 	private static final Image sprite_oeuf = new Image("/images/sprites/sprite_oeuf.png");
 	@FXML private Label life, sante, jnom;
+	@FXML private ImageView affichage;
+	
+	
+	private static final Image IMAGE = new Image("/images/sprites/sprite_chien.png");
 	
 	@FXML private void initialize(){
 		sante.setText(String.valueOf(Main.tama.getSante()));
-		jnom.setText(Main.tama.getNom());
+		jnom.setText(Main.tama.getNom().substring(0, 4));
+		Image sprite;
+		if (Main.tama.getRace() == Caracteristique.CHIEN) {
+			sprite = sprite_chien;
+		}else if (Main.tama.getRace() == Caracteristique.CHAT) {
+			sprite = sprite_chat;
+		}else {
+			sprite = sprite_oiseau;
+		}
+
+		affichage.setImage(sprite_bebe);
+		Animation animation_bebe = new SpriteAnimation(affichage, Duration.millis(1000), 
+				COUNT, Position.COLUMNS_bebe, 
+				Position.OFFSET_X_bebe_dormir, Position.OFFSET_Y_bebe_dormir,
+                WIDTH, HEIGHT
+        );
+		animation_bebe.setCycleCount(Animation.INDEFINITE);
+		animation_bebe.play();
 	}
 }
---
+++ src/application/Position.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Position {
+	public static final int COLUMNS_bebe  =   3;
+	public static final int COUNT_bebe    =  3;
+	public static final int OFFSET_X_bebe =  140;
+	public static final int OFFSET_Y_bebe =  50;
+	public static final int OFFSET_X_bebe_sommeil =  470;
+	public static final int OFFSET_Y_bebe_sommeil =  90;
+	public static final int OFFSET_X_bebe_dormir =  470;
+	public static final int OFFSET_Y_bebe_dormir =  135;
+
+	public static final int WIDTH = 40;
+	public static final int HEIGHT = 40;
+}
